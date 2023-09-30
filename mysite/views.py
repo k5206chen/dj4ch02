a3b66b9370678233f2b77c167b07f989e369803c
k5206chen <k5206chen@gmail.com>
2023-10-01 00:04:42 +0800

    20231001

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from mysite.models import Post
+from django.http import HttpResponse
+
+def homepage(request):
+    posts = Post.objects.all()
+    post_lists = list()
+    for count, post in enumerate(posts):
+        post_lists.append(f"No... {count}: {post} <br>")
+        #         post_lists.append("No. {}:".format(str(count)) + str(post) + "<br>")
+    return HttpResponse(post_lists)
 
 # Create your views here.
